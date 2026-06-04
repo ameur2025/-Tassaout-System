b946801ac7b3a6ca2b658321702a5c9afee2b2ef
Sraghna services <marocinvest2012@gmail.com>
2026-06-04 17:39:02 +0100

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,10 @@
-https://github.com/ameur2025/-Tassaout-System/edit/main/js/admin.js
+// Alpha Core Nexus DANA - النواة البرمجية
+console.log("نظام تساوت العقاري: النواة الرقمية في حالة استجابة.");
+
+function initializeSystem() {
+    console.log("جاري تحميل بروتوكولات الإدارة...");
+    alert("مرحباً بك في نظام تساوت - المصنع البرمجي نشط!");
+}
+
+// تشغيل النظام
+initializeSystem();
